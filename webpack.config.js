46f119ceb76172f89e54038595e9a889924b6b4c
hojeongpark <hojeong.park@creatrip.com>
2022-01-08 16:59:24 +0900

    webpack 설치 및 webpack 설정
    - entry, output, mode 설정

---
+++ webpack.config.js
@@ -0,0 +1,12 @@
+var path = require('path');
+
+module.exports = {
+    mode: "development",
+    entry: {
+        main: "./src/app.js",
+    },
+    output: {
+        path: path.resolve(__dirname, './dist'),
+        filename: "[name].js"
+    }
+}
